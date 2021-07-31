44cab27d134ae6bce2a44023d4249cd35e992258
juandiegovg <67924398+juanvg1529@users.noreply.github.com>
2021-07-31 11:44:36 -0500

    changes on results changing the show preview gallery gifs

--- src/Components/Results/Results.jsx
+++ src/Components/Results/Results.jsx
@@ -2,8 +2,26 @@ import React, { useContext } from "react";
 import { AppContext } from "../../Context/AppContext";
 import "./Results.css";
 
+const endpoint=  "https://api.giphy.com/v1/gifs/search?api_key=OBBzzXm9g5kiOcuF9MgaX4zTuCHy1t7j&q=dog&limit=25&offset=0&rating=g&lang=en";
+
 function Search() {
 
+  async function renderGiphos(){
+    try{
+      const request =await fetch(endpoint);
+      const app=document.getElementById("root");
+      const json=await request.json();
+      json.data.array.forEach((gif) => {
+        const image= document.createElement("img");
+        image.src=gif.image.fixed_height.url;
+        app.appendChild(image)
+        
+      });
+    }
+    catch(error){
+      console.log("nada, paila");
+    }
+  }
   const {showGalery,setShowGalery} =useContext(AppContext)
   const onclickButton=()=>{return 1}
    
@@ -12,8 +30,8 @@ function Search() {
   // );
   return (
     <div className="Results-component">
-      <section className="Search-Title"></section>
       <button onClick={onclickButton} className="Search-Button"> Resultados de la busqueda </button>
+    
     </div>
   );
 }

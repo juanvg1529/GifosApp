db613c1c79cd01e29ab5948c97b1ceda65ee07fd
juandiegovg <67924398+juanvg1529@users.noreply.github.com>
2021-08-12 15:07:29 -0500

    enter event done

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,38 +1,52 @@
 import React, { useContext, useEffect } from "react";
 import "./SearchStyles/Search.css";
-import "./SearchStyles/DarkSearch.css"
-import {AppContext} from "../../Context/AppContext"
+import "./SearchStyles/DarkSearch.css";
+import { AppContext } from "../../Context/AppContext";
 import { DarkModeHook } from "../../Darkmode/DarkmodeHook";
 
 export function Search() {
-  
-  const {isDarkMode,stateButton,setStateButton,searchState,setSearchState}=useContext(AppContext);
+  const {
+    isDarkMode,
+    stateButton,
+    setStateButton,
+    searchState,
+    setSearchState,
+  } = useContext(AppContext);
 
   //dark mode
-  const searchBackground=`Search-component ${isDarkMode?"dark":"ligth"}`;
-  const SearchBar=`SearchBAR ${isDarkMode?"dark":"ligth"}`;
-  const searchButton=`Search-button ${isDarkMode?"dark":"ligth"}`;
-  const searchTitle=`Title-Search ${isDarkMode?"dark":"ligth"}`;
-  
-  
-    //buton function to implement the search
-  const searchButtonFunction=()=>{
-    setStateButton(!stateButton)
+  const searchBackground = `Search-component ${isDarkMode ? "dark" : "ligth"}`;
+  const SearchBar = `SearchBAR ${isDarkMode ? "dark" : "ligth"}`;
+  const searchButton = `Search-button ${isDarkMode ? "dark" : "ligth"}`;
+  const searchTitle = `Title-Search ${isDarkMode ? "dark" : "ligth"}`;
+
+  //buton function to implement the search
+  const searchButtonFunction = () => {
+    setStateButton(!stateButton);
     console.log(stateButton);
-    
   };
-const onChangeSearch=(e)=>setSearchState(e.target.value);
+  const onChangeSearch = (e) => setSearchState(e.target.value);
 
-  useEffect( //trying use effect
-   ()=>{
-    console.log("the botton has been clicked");
-   },[searchButtonFunction]
-
+  useEffect(
+    //trying use effect
+    () => {
+      console.log("the botton has been clicked");
+    },
+    [searchButtonFunction]
   );
+
+  const searchKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      setStateButton(!stateButton);
+    }
+  };
+
   return (
     <div className={DarkModeHook("Search-component")}>
       <section className={DarkModeHook("Title-Search")}>
-        <h2 > ¡Inspirate y busca los mejores <b>GIFS </b>!</h2>
+        <h2>
+          {" "}
+          ¡Inspirate y busca los mejores <b>GIFS </b>!
+        </h2>
       </section>
       <section>
         <img
@@ -48,19 +62,20 @@ const onChangeSearch=(e)=>setSearchState(e.target.value);
           id="gsearch"
           name="gsearch"
           value={searchState}
-          placeholder= "Busca gifs"
+          placeholder="Busca gifs"
           onChange={onChangeSearch}
           onClick={searchButtonFunction}
+          onKeyDown={searchKeyPress}
         ></input>
         <button
-        onClick={searchButtonFunction}
-        className={DarkModeHook("Search-button")}>
-        
+          onClick={searchButtonFunction}
+          className={DarkModeHook("Search-button")}
+        >
           <img src="../../utils/Vector.png" alt="lupa"></img>
         </button>
       </section>
-      <section> 
-       <p className={"Search-Results"}>Search Results</p> 
+      <section>
+        <p className={"Search-Results"}>Search Results</p>
       </section>
     </div>
   );

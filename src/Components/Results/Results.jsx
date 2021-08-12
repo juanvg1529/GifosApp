4cde6022dde69667c9ef6d3c28628e501e9bc320
juandiegovg <67924398+juanvg1529@users.noreply.github.com>
2021-08-12 16:58:47 -0500

    loading text done

--- src/Components/Results/Results.jsx
+++ src/Components/Results/Results.jsx
@@ -6,29 +6,33 @@ import { CardGifo } from "../CardGifo/CardGifo";
 import { DarkModeHook } from "../../Darkmode/DarkmodeHook";
 
 function Results() {
-  const { stateButton, setStateButton, gifoState, setGifoState,searchState,setSearchState } =
-    useContext(AppContext);
+  const {
+    stateButton,
+    setStateButton,
+    gifoState,
+    setGifoState,
+    searchState,
+    textInfo,
+    setTextInfo,
+  } = useContext(AppContext);
   //console.log(gifoState);
 
   useEffect(() => {
     if (stateButton) {
+      setTextInfo("Loading...");
       async function apiGiphyrRequest() {
         try {
+
           const request = await apiRequest(searchState);
           const dataGif = await request.json();
           setGifoState(dataGif.data);
           setStateButton(false);
-          // console.log(dataGif.data);
-
-          //   );
 
           if (dataGif.data.length === 0) {
-            console.log("paila nea");
-          } else {
-            console.log("data no es cero");
+            setTextInfo(`sorry we can't find ${searchState}`);
           }
         } catch (error) {
-          console.log("nada, paila");
+          setTextInfo("Ups something when wrong, please try again :( ");
         }
       }
       apiGiphyrRequest();
@@ -44,7 +48,7 @@ function Results() {
 
   return (
     <div className={DarkModeHook("Results-component")}>
-      {gifoState.length > 0 ? gifosRender : <p>paila</p>}
+      {gifoState.length > 0 ? gifosRender : <p>{textInfo}</p>}
     </div>
   );
 }
--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -9,11 +9,12 @@ export function AppProvider(props){
     const[stateButton,setStateButton]=useState(false);
     const[gifoState,setGifoState]=useState([]);
     const [searchState,setSearchState]=useState("");
+    const [textInfo,setTextInfo]=useState("Do some gif search!");
    
 
  return(
      
-     <AppContext.Provider value={{isDarkMode,setIsDarkMode,stateButton,setStateButton,gifoState,setGifoState,searchState,setSearchState}}>
+     <AppContext.Provider value={{isDarkMode,setIsDarkMode,stateButton,setStateButton,gifoState,setGifoState,searchState,setSearchState,textInfo,setTextInfo}}>
         {props.children}
     </AppContext.Provider>
          )   

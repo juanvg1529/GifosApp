df1d1bc646240c14b4d3873cd2ff9c675b0adaa9
juandiegovg <67924398+juanvg1529@users.noreply.github.com>
2021-08-12 18:53:27 -0500

    working autocomplete

---
+++ src/Components/Autocomplete/Autocomplete.jsx
@@ -0,0 +1,10 @@
+import { DarkModeHook } from "../../Darkmode/DarkmodeHook"
+
+
+export const Autocomplete=(props)=>{
+
+    <div className={DarkModeHook("AutoComplete-Component")}>
+        <p>{props.sugestion}</p>
+
+    </div>
+}
